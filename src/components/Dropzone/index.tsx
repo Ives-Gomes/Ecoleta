877a9ac69570ec064b393763f894991e7e503b0a
Ives <ivesmoreira3@gmail.com>
2020-06-05 14:30:13 -0300

    Dropzone for image

---
+++ src/components/Dropzone/index.tsx
@@ -0,0 +1,49 @@
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+
+import './styles.css';
+
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const fileUrl = URL.createObjectURL(file);
+
+      setSelectedFileUrl(fileUrl);
+      onFileUploaded(file);
+    },
+    [onFileUploaded],
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+  });
+
+  return (
+    <div className='dropzone' {...getRootProps()}>
+      <input {...getInputProps()} accept='image/*' />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt='Point thumbnail' />
+      ) : isDragActive ? (
+        <p>
+          <FiUpload /> Solte as imagens aqui...
+        </p>
+      ) : (
+        <p>
+          <FiUpload /> Clique ou solte aqui as imagens do estabelecimento.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Dropzone;
